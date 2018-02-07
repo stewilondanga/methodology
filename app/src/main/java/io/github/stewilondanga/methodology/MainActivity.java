2a869bdd87482ec95e4eb0746144352078f0cec6
Stewart ilondanga <stilondanga@gmail.com>
2018-02-07 12:39:17 +0300

    add app/src/main/java/io/github/stewilondanga/methodology/MainActivity.java

--- app/src/main/java/io/github/stewilondanga/methodology/MainActivity.java
+++ app/src/main/java/io/github/stewilondanga/methodology/MainActivity.java
@@ -2,12 +2,19 @@ package io.github.stewilondanga.methodology;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+        TextView textView = new TextView(this);
+        textView.setText("Wow");
+        textView.setTextSize(56);
+        textView.setMaxLines(2);
+
+        setContentView(textView);
     }
 }
